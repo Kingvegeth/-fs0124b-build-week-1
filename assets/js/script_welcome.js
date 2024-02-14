8718b53a170ceef479c52483c35839b1b99e9f26
kingvegeth <kingvegthpc@gmail.com>
2024-02-14 15:22:03 +0100

    Css finito

--- assets/js/script_welcome.js
+++ assets/js/script_welcome.js
@@ -3,9 +3,13 @@ const button = document.querySelector(".proceed_button");
 const alerta = document.querySelector(".alert");
 
 check.addEventListener("click", () => {
-  if (check.classList.contains("clicked")) check.classList.remove("clicked");
+  if (check.classList.contains("clicked")) {
+    check.classList.remove("clicked");
+    check.innerHTML =''
+  }
   else {
     check.classList.add("clicked");
+    check.innerHTML = '<ion-icon name="checkmark-outline" class="checkmark"></ion-icon>';
     alerta.classList.add("hidden");
   }
 });
@@ -14,6 +18,6 @@ button.addEventListener("click", (e) => {
   e.preventDefault()
   if (!check.classList.contains("clicked")) alerta.classList.remove("hidden");
   else{
-    
+    window.location = "quiz.html";
   }
 });
